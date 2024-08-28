b111cdb3739fc3be2a7a9c70d6a61314101f7518
roderick <roderickalbertop@gmail.com>
2024-08-28 15:54:39 -0600

    texto en pagina web

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  #protect_from_forgery with: :exceptions
+  def texto
+    render html:"bienvenido a mi sitio web!"
+  end
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  #allow_browser versions: :modern
+end
